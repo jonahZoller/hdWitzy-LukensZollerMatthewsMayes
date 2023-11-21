8cf2998d0ff644a6b991feb8b9101a6aba46b3d5
Jonah <jonahzoller@gmail.com>
2023-11-21 13:34:15 -0600

    inital menu push

--- HDWitzys/app/src/main/java/com/example/hdwitzys/ui/menu/MenuViewModel.java
+++ HDWitzys/app/src/main/java/com/example/hdwitzys/ui/menu/MenuViewModel.java
@@ -10,7 +10,7 @@ public class MenuViewModel extends ViewModel {
 
     public MenuViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is the menu fragment");
     }
 
     public LiveData<String> getText() {
