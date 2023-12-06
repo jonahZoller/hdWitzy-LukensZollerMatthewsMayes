4f14fcc6ac30965122292b65a25417638119efc5
unknown <2smallgarage@gmail.com>
2023-12-05 18:23:28 -0600

    WitzysBuild

--- HDWitzys/app/src/main/java/com/example/hdwitzys/ui/menu/MenuFragment.java
+++ HDWitzys/app/src/main/java/com/example/hdwitzys/ui/menu/MenuFragment.java
@@ -21,9 +21,12 @@ public class MenuFragment extends Fragment {
 
     private FragmentMenuBinding binding;
 
-    private Button combos, sandwich, specialties, hotdogs, desserts, salads;
-    private ScrollView scroll, comboSV, sandwichSV;
-    private CheckBox c1, c2, c3, c4, c5, c6, c7;
+    private Button combos, sandwich, specialties, hotdogs, desserts, salads, back, back2, back3, back4, back5;
+    private ScrollView scroll, comboSV, sandwichSV, specialtySV, hotdogSV, dessertSV, saladSV;
+    private CheckBox c1, c2, c3, c4, c5, c6, c7, s1, s2, s3 ,s4, s5, sp1, sp2, sp3, h1, h2, h3, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, isplit, ix2, ix3, pint, quart, shake;
+    //all of the letters correspond to where the checkboxes are held
+    // c = combos | s = sandwiches | sp = specialties | h = hotdogs | i = icecream | sa = salads
+    //isplit 
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -45,9 +48,21 @@ public class MenuFragment extends Fragment {
         @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
         ArrayList<String> saladList = new ArrayList<String>();
 
+        //scroll views (these separate the categories by food)
         scroll = rootView.findViewById(R.id.sv1);
         comboSV = rootView.findViewById(R.id.sv2);
         sandwichSV = rootView.findViewById(R.id.sv3);
+        specialtySV = rootView.findViewById(R.id.sv4);
+        hotdogSV = rootView.findViewById(R.id.sv5);
+        dessertSV = rootView.findViewById(R.id.sv6);
+
+
+        //buttons
+        back = rootView.findViewById(R.id.btnBack);
+        back2 = rootView.findViewById(R.id.btnBack2);
+        back3 = rootView.findViewById(R.id.btnBack3);
+        back4 = rootView.findViewById(R.id.btnBack4);
+        back5 = rootView.findViewById(R.id.btnBack5);
 
         sandwich = rootView.findViewById(R.id.sandwichBTN);
         combos = rootView.findViewById(R.id.comboBTN);
@@ -60,6 +75,9 @@ public class MenuFragment extends Fragment {
         scroll.setVisibility(View.VISIBLE);
         comboSV.setVisibility(View.GONE);
         sandwichSV.setVisibility(View.GONE);
+        specialtySV.setVisibility(View.GONE);
+        hotdogSV.setVisibility(View.GONE);
+        dessertSV.setVisibility(View.GONE);
         //setting combo visible
 
 
@@ -129,34 +147,102 @@ public class MenuFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 scroll.setVisibility(View.GONE);
-
+                specialtySV.setVisibility(View.VISIBLE);
             }
         });
 
         hotdogs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                scroll.setVisibility(View.GONE);
+                hotdogSV.setVisibility(View.VISIBLE);
             }
         });
 
         desserts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                scroll.setVisibility(View.GONE);
+                dessertSV.setVisibility(View.VISIBLE);
             }
         });
 
         salads.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                scroll.setVisibility(View.GONE);
+                saladSV.setVisibility(View.VISIBLE);
             }
         });
 
 
-        //stuff at the bottom, idk what these do though. --- Ben
+        //back buttons to return to the menus
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scroll.setVisibility(View.VISIBLE);
+                comboSV.setVisibility(View.GONE);
+                sandwichSV.setVisibility(View.GONE);
+                specialtySV.setVisibility(View.GONE);
+                hotdogSV.setVisibility(View.GONE);
+                dessertSV.setVisibility(View.VISIBLE);
+                saladSV.setVisibility(View.VISIBLE);
+            }
+        });
 
+        back2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scroll.setVisibility(View.VISIBLE);
+                comboSV.setVisibility(View.GONE);
+                sandwichSV.setVisibility(View.GONE);
+                specialtySV.setVisibility(View.GONE);
+                hotdogSV.setVisibility(View.GONE);
+                dessertSV.setVisibility(View.VISIBLE);
+                saladSV.setVisibility(View.VISIBLE);
+            }
+        });
+
+        back3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scroll.setVisibility(View.VISIBLE);
+                comboSV.setVisibility(View.GONE);
+                sandwichSV.setVisibility(View.GONE);
+                specialtySV.setVisibility(View.GONE);
+                hotdogSV.setVisibility(View.GONE);
+                dessertSV.setVisibility(View.VISIBLE);
+                saladSV.setVisibility(View.VISIBLE);
+            }
+        });
+
+        back4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scroll.setVisibility(View.VISIBLE);
+                comboSV.setVisibility(View.GONE);
+                sandwichSV.setVisibility(View.GONE);
+                specialtySV.setVisibility(View.GONE);
+                hotdogSV.setVisibility(View.GONE);
+                dessertSV.setVisibility(View.VISIBLE);
+                saladSV.setVisibility(View.VISIBLE);
+            }
+        });
+
+        back5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scroll.setVisibility(View.VISIBLE);
+                comboSV.setVisibility(View.GONE);
+                sandwichSV.setVisibility(View.GONE);
+                specialtySV.setVisibility(View.GONE);
+                hotdogSV.setVisibility(View.GONE);
+                dessertSV.setVisibility(View.VISIBLE);
+                saladSV.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //stuff at the bottom, idk what these do though. --- Ben
         return rootView;
     }
 
